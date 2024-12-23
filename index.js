45481028354107970df25f8a16f79552ef5254f0
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2024-12-23 23:45:35 +0700

    feat: after getting user location it redirect and sent the data to "/" page

--- index.js
+++ index.js
@@ -10,20 +10,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-    res.render("index.ejs", {
-        user_latitude: null,
-        user_longitude: null 
-       });
+    var location = null;
 
+    if (req.query.latitude && req.query.longitude) {
+        location = { latitude: req.query.latitude, longitude: req.query.longitude };
+    }
+
+    res.render("index.ejs", { user_location: location });
+
+    console.log(location);
 });
 
 app.post("/location", (req, res) => {
     const { latitude, longitude } = req.body;
     console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
-    res.render("index.ejs", {
-         user_latitude: latitude,
-         user_longitude: longitude 
-        });
+    res.redirect(`/?latitude=${encodeURIComponent(latitude)}&longitude=${encodeURIComponent(longitude)}`);
 });
 
 app.listen(port, () => {
