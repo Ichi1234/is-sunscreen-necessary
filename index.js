5d61a54d183010f6ed1ba0d32a88fdaf47d4d8fd
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2024-12-23 23:24:30 +0700

    feat: Send the longitute and latitude to the backend

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 import express from "express";
 import axios from "axios";
 
+
 const app = express();
 const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {
+        user_latitude: null,
+        user_longitude: null 
+       });
 
+});
+
+app.post("/location", (req, res) => {
+    const { latitude, longitude } = req.body;
+    console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+    res.render("index.ejs", {
+         user_latitude: latitude,
+         user_longitude: longitude 
+        });
 });
 
 app.listen(port, () => {
